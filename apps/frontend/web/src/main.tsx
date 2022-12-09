28aa608e66907697abaa43169ca6bc00c5d320e2
Ysedda <arthur.baudoin@icloud.com>
2022-12-09 11:32:48 +0100

    chore: removed css files and imports, moved chakra theme in its own folder

--- apps/frontend/web/src/main.tsx
+++ apps/frontend/web/src/main.tsx
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { extendTheme } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-import './index.css';
-
-const config = {
-  initialColorMode: 'light',
-  useSystemColorMode: false,
-};
-
-const theme = extendTheme({ config });
+import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
