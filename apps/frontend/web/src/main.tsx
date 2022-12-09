c41ba15d41ba76242109253824b2d1fcb2eb4039
MGliem <maximilian.gliem@gmail.com>
2022-12-09 10:17:33 +0100

    feat(frontend): conf chakra theme system color to false

--- apps/frontend/web/src/main.tsx
+++ apps/frontend/web/src/main.tsx
@@ -1,12 +1,20 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import './index.css';
 
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
