f27ead8f53ff23eb217eecaa16a5c825ddef51fd
Ysedda <arthur.baudoin@icloud.com>
2022-12-13 15:47:54 +0100

    chore: renamed routes file

--- apps/frontend/web/src/main.tsx
+++ apps/frontend/web/src/main.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import theme from './theme';
 import { RouterProvider } from 'react-router-dom';
-import router from './routes/Routes';
+import router from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
