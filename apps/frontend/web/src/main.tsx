8aa1497ee4b31bba9684c891b7d16ba2ce867888
Ysedda <arthur.baudoin@icloud.com>
2022-12-13 14:53:48 +0100

    feat: react-router-dom install and setup

--- apps/frontend/web/src/main.tsx
+++ apps/frontend/web/src/main.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { extendTheme } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import theme from './theme';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
