1e8d2ebf6196fac876e5bcabd51a3b46dbd357cf
MGliem <maximilian.gliem@gmail.com>
2022-12-15 12:28:59 +0100

    feat: added lexend deca font

--- apps/frontend/web/src/theme/index.ts
+++ apps/frontend/web/src/theme/index.ts
@@ -1,10 +1,16 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
 
 const config = {
-    initialColorMode: 'light',
-    useSystemColorMode: false,
-  };
-  
-const theme = extendTheme({ config });
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({
+  config,
+  fonts: {
+    heading: `'Lexend Deca', sans-serif`,
+    body: `'Lexend Deca', sans-serif`,
+  },
+});
 
 export default theme;
