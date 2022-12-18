4991738442522f662226b7bffb2fa4e4d5fccad1
Ysedda <arthur.baudoin@icloud.com>
2022-12-18 12:02:09 +0100

    feat(front): form part 2 WIP

--- apps/frontend/web/src/components/forms/trip/RouteDetails.tsx
+++ apps/frontend/web/src/components/forms/trip/RouteDetails.tsx
@@ -1,42 +1,49 @@
-import { Button, FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react"
-import { useFormContext } from "react-hook-form";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Radio,
+  Stack,
+} from '@chakra-ui/react';
+import { useFormContext } from 'react-hook-form';
 
 const RouteDetails = () => {
   const { register } = useFormContext();
-  const  errors  = useFormContext().formState.errors;
+  const errors = useFormContext().formState.errors;
   const isSubmitting = useFormContext().formState.isSubmitting;
 
   return (
     <>
-    <FormControl
-      isInvalid={Boolean(errors.yolo)}
-      marginTop={'18'}
-      width={'500px'}
-    >
-      <FormLabel htmlFor="yolo" fontSize={'24'} textAlign={'center'}>
-        Where are you going to?
-      </FormLabel>
-      <Input
-        height={'70px'}
-        id="yolo"
-        placeholder="Chalet du steak"
-        fontSize={'24'}
-        {...register('yolo', {
-          required: 'This is required',
-          minLength: { value: 4, message: 'Minimum length should be 4' },
-          validate: (value) => { return !!value.trim()}
-        })}
-      />
-      <FormErrorMessage>
-        
-        {errors?.yolo && errors.yolo.message?.toString()}
-      </FormErrorMessage> 
-    </FormControl>
-    <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
-      Next
-    </Button>
-  </>
-  )
-}
+      <FormControl
+        isInvalid={Boolean(errors.yolo)}
+        marginTop={'18'}
+        width={'500px'}
+      >
+        <FormLabel htmlFor="yolo" fontSize={'24'} textAlign={'center'}>
+          What is your route?
+        </FormLabel>
+        <Stack>
+          <Radio size="lg" name="1" colorScheme="#394E61">
+            Radio
+          </Radio>
+          <Radio size="lg" name="2" colorScheme="#394E61">
+            Radio
+          </Radio>
+          <Radio size="lg" name="3" colorScheme="#394E61">
+            Radio
+          </Radio>
+        </Stack>
+        <FormErrorMessage>
+          {errors?.yolo && errors.yolo.message?.toString()}
+        </FormErrorMessage>
+      </FormControl>
+      <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
+        Next
+      </Button>
+    </>
+  );
+};
 
-export default RouteDetails
+export default RouteDetails;
