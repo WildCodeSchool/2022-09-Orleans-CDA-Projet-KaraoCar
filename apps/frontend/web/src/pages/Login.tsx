31e5379f7dc1bea4c0aa98a200bf537227577253
Dangiras <dan.hausmans@gmail.com>
2022-12-29 15:28:24 +0100

    feat(frontend)Form styles put into place part 1

--- apps/frontend/web/src/pages/Login.tsx
+++ apps/frontend/web/src/pages/Login.tsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Image,
+  extendTheme,
+  Center,
+  Text,
+} from '@chakra-ui/react';
 
 const Login = () => {
   const [username, setusername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleSubmit = (event: any) => {
+  /*const handleSubmit = (event: any) => {
+    event.preventDefault(); */
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     fetch('http://localhost:3333/api/auth/login', {
@@ -14,21 +29,22 @@ const Login = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-    }).then(response => response.json()
-    ).then((data) => {
-      console.log(data);
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
         if (data.token) {
           localStorage.setItem('token', data.token);
-          console.log("SUCCESS")
+          console.log('SUCCESS');
           // TODO: redirection to another page
         } else {
-          console.log("ERROR")
+          console.log('ERROR');
           // on fail
           setError(data.error);
         }
       });
   };
-
+  /* 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="username">username:</label>
@@ -50,6 +66,113 @@ const Login = () => {
       {error && <div className="error">{error}</div>}
       <button type="submit">Log in</button>
     </form>
+  );
+};
+*/
+  /*
+    <form onSubmit={handleSubmit}>
+      <Stack>
+        <FormControl>
+          <FormLabel htmlFor="username">username</FormLabel>
+          <Input
+            id="username"
+            type="username"
+            value={username}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setusername(event.target.value)
+            }
+            placeholder="Enter your email"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(event.target.value)
+            }
+            placeholder="Enter your password"
+          />
+        </FormControl>
+        {error && <div>{error}</div>}
+        <Button type="submit" mt={4}>
+          Login
+        </Button>
+      </Stack>
+    </form>
+  )*/
+  return (
+    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+      <Flex p={10} flex={2} align={'center'} justify={'center'}>
+        <Stack spacing={20} w={'full'} maxW={'md'}>
+          <Heading fontSize={'50px'} marginBottom={'5rem'} alignSelf={'center'}>
+            Log in to your account
+          </Heading>
+          <FormControl id="email">
+            <FormLabel fontSize={'25px'}>Email address</FormLabel>
+            <Input
+              rounded={'8px'}
+              border={'1px solid #000000'}
+              backgroundColor={'#fff'}
+              borderColor={'black'}
+              height={'50px'}
+              width={'500px'}
+              fontSize={'25px'}
+              type="username"
+            />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel fontSize={'25px'}>Password</FormLabel>
+            <Input
+              rounded={'8px'}
+              border={'1px solid #000000'}
+              height={'50px'}
+              width={'500px'}
+              fontSize={'25px'}
+              type="password"
+            />
+          </FormControl>
+          <Stack spacing={6}>
+            <Button
+              marginTop={'2rem'}
+              height={'40px'}
+              width={'300px'}
+              rounded={'8px'}
+              type="submit"
+              backgroundColor={'#394E61'}
+              textColor={'#fff'}
+              variant="solid"
+              alignSelf={'center'}
+            >
+              Login
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1} backgroundColor={'#E4F2FF'}>
+        <Heading fontSize={'40px'} alignSelf={'center'} >
+          New Here?
+        </Heading>
+        <Text alignSelf={'center'}>
+          Sign up and discover the joys of singing during your trip
+        </Text>
+        <Button
+          border={'1px solid #000000'}
+          backgroundColor={'#fff'}
+          marginTop={'2rem'}
+          height={'40px'}
+          width={'300px'}
+          rounded={'8px'}
+          type="submit"
+          variant="solid"
+          alignSelf={'center'}
+        >
+          Sign Up
+        </Button>
+      </Flex>
+    </Stack>
   );
 };
 
--- apps/frontend/web/src/pages/SignUp.tsx
+++ apps/frontend/web/src/pages/SignUp.tsx
@@ -1,4 +1,18 @@
 import React, { useState } from 'react';
+import {
+  Grid,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Image,
+  extendTheme,
+  Center,
+  Text,
+} from '@chakra-ui/react';
 
 function SignupForm() {
   const [email, setEmail] = useState('');
@@ -29,7 +43,7 @@ function SignupForm() {
       });
   };
 
-  return (
+  /* return (
     <form onSubmit={handleSubmit}>
       {error && <p>{error}</p>}
       <label htmlFor="email">Email:</label>
@@ -51,6 +65,121 @@ function SignupForm() {
       <button type="submit">Sign Up</button>
     </form>
   );
-}
+} */
+return (
+    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+      <Flex flex={1} backgroundColor={'#E4F2FF'}>
+        <Heading fontSize={'40px'} alignSelf={'center'} >
+          Already have an account?
+        </Heading>
+        <Button
+          border={'1px solid #000000'}
+          backgroundColor={'#fff'}
+          marginTop={'2rem'}
+          height={'40px'}
+          width={'300px'}
+          rounded={'8px'}
+          type="submit"
+          variant="solid"
+          alignSelf={'center'}
+        >
+          Login
+        </Button>
+      </Flex>
+      <Flex p={10} flex={2} align={'center'} justify={'center'}>
+        <Stack spacing={20} w={'full'} maxW={'md'}>
+          <Heading fontSize={'50px'} marginBottom={'5rem'} alignSelf={'center'}>
+            Create Your Account
+          </Heading>
+          <Grid templateColumns="1fr 1fr" gap={6}>
+          <FormControl id="email">
+            <FormLabel fontSize={'25px'}>Firstname</FormLabel>
+            
+            <Input
+              rounded={'8px'}
+              border={'1px solid #000000'}
+              backgroundColor={'#fff'}
+              borderColor={'black'}
+              height={'50px'}
+              width={'350px'}
+              fontSize={'25px'}
+              type="username"
+            />
+          </FormControl>
+          <FormControl id="email">
+            <FormLabel fontSize={'25px'}>Lastname</FormLabel>
+            
+            <Input
+              rounded={'8px'}
+              border={'1px solid #000000'}
+              backgroundColor={'#fff'}
+              borderColor={'black'}
+              height={'50px'}
+              width={'350px'}
+              fontSize={'25px'}
+              type="username"
+            />
+          </FormControl>
+      </Grid>
+          <FormControl id="email">
+            <FormLabel fontSize={'25px'}>Email address</FormLabel>
+            
+            <Input
+              rounded={'8px'}
+              border={'1px solid #000000'}
+              backgroundColor={'#fff'}
+              borderColor={'black'}
+              height={'50px'}
+              width={'700px'}
+              fontSize={'25px'}
+              type="username"
+            />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel fontSize={'25px'}>Password</FormLabel>
+            <Input
+              rounded={'8px'}
+              border={'1px solid #000000'}
+              height={'50px'}
+              width={'700px'}
+              fontSize={'25px'}
+              type="password"
+            />
+          </FormControl>
+          <FormControl id="email">
+            <FormLabel fontSize={'25px'}>Confirm Password</FormLabel>
+            
+            <Input
+              rounded={'8px'}
+              border={'1px solid #000000'}
+              backgroundColor={'#fff'}
+              borderColor={'black'}
+              height={'50px'}
+              width={'700px'}
+              fontSize={'25px'}
+              type="username"
+            />
+          </FormControl>
+          <Stack spacing={6}>
+            <Button
+              marginTop={'2rem'}
+              height={'40px'}
+              width={'300px'}
+              rounded={'8px'}
+              type="submit"
+              backgroundColor={'#394E61'}
+              textColor={'#fff'}
+              variant="solid"
+              alignSelf={'center'}
+            >
+              Create your account
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      
+    </Stack>
+  );
+};
 
 export default SignupForm;
