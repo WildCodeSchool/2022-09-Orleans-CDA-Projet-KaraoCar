289ff61085f1be277a282e063ca67d3372ad097e
Ysedda <arthur.baudoin@icloud.com>
2023-01-03 12:59:33 +0100

    feat(backend): validator installation and trip validations

--- apps/backend/api/src/trips/dto/create-trip.dto.ts
+++ apps/backend/api/src/trips/dto/create-trip.dto.ts
@@ -1,8 +1,18 @@
+import { IsArray, IsBoolean, IsNotEmpty, IsNumber } from 'class-validator';
 import { MusicalStyle } from 'src/musical-styles/entities/musical-style.entity';
 
 export class CreateTripDto {
+  @IsNotEmpty()
+  @IsNumber()
   carId: number;
+
+  @IsNotEmpty()
+  @IsBoolean()
   middleseat: boolean;
+
+  @IsNotEmpty()
+  @IsArray()
   musicalStyles: MusicalStyle[];
+
   comment: string;
 }
