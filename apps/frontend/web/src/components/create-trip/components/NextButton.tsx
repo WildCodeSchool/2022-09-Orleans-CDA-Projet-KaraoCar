62f2aa2f8968f53cf354475cf87af195a00b99eb
Ysedda <arthur.baudoin@icloud.com>
2023-01-03 11:12:58 +0100

    style: previous and next buttons icons

--- apps/frontend/web/src/components/create-trip/components/NextButton.tsx
+++ apps/frontend/web/src/components/create-trip/components/NextButton.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@chakra-ui/react';
+import { Button, Text } from '@chakra-ui/react';
 import { useFormContext } from 'react-hook-form';
-import { GrFormNext } from 'react-icons/gr';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 const NextButton = () => {
   const isSubmitting = useFormContext().formState.isSubmitting;
   return (
     <Button
-      width={{ base: '125px', md: '160px' }}
+      width={{ base: '130px', md: '160px' }}
       height={{ base: '50px', md: '60px' }}
       backgroundColor={'#394E61'}
       color={'#FEFEFE'}
@@ -14,26 +14,10 @@ const NextButton = () => {
       type="submit"
       fontSize={{ base: '18', md: '22' }}
       shadow={'0px 2px 2px rgba(0, 0, 0, 0.25)'}
-      rightIcon={
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          stroke-width="0"
-          viewBox="0 0 24 24"
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <polyline
-            fill="none"
-            stroke="#FEFEFE"
-            stroke-width="2"
-            points="9 6 15 12 9 18"
-          ></polyline>
-        </svg>
-      }
+      gap={'2'}
     >
-      {'Next'}
+      <Text as={'span'}>{'Next'}</Text>
+      <BsArrowRightShort size={'30'} />
     </Button>
   );
 };
--- apps/frontend/web/src/components/create-trip/components/PreviousButton.tsx
+++ apps/frontend/web/src/components/create-trip/components/PreviousButton.tsx
@@ -1,5 +1,6 @@
-import { Button } from '@chakra-ui/react';
+import { Button, Text } from '@chakra-ui/react';
 import { Dispatch, SetStateAction } from 'react';
+import { BsArrowLeftShort } from 'react-icons/bs';
 
 const PreviousButton = ({
   step,
@@ -11,34 +12,17 @@ const PreviousButton = ({
   return (
     <Button
       variant={'outline'}
-      width={{ base: '125px', md: '160px' }}
+      width={{ base: '130px', md: '160px' }}
       height={{ base: '50px', md: '60px' }}
       backgroundColor={'#FEFEFE'}
       onClick={() => setStep(step - 1)}
       fontSize={{ base: '18', md: '22' }}
       border={'2px solid #394E61'}
       shadow={'0px 2px 2px rgba(0, 0, 0, 0.25)'}
-      leftIcon={
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          stroke-width="0"
-          viewBox="0 0 24 24"
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <polyline
-            fill="none"
-            stroke="#394E61"
-            stroke-width="2"
-            points="9 6 15 12 9 18"
-            transform="matrix(-1 0 0 1 24 0)"
-          ></polyline>
-        </svg>
-      }
+      padding={'0'}
     >
-      {'Previous'}
+      <BsArrowLeftShort size={'30'} />
+      <Text as={'span'}>{'Previous'}</Text>
     </Button>
   );
 };
