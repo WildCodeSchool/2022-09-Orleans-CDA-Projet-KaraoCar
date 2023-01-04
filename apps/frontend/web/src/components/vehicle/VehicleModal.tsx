1ef5545a71060c6644e2f7a138ebaf81f6604a1f
Dangiras <dan.hausmans@gmail.com>
2023-01-04 12:14:48 +0100

    feat(frontend): Finished form design on the frontend

--- apps/frontend/web/src/components/vehicle/VehicleModal.tsx
+++ apps/frontend/web/src/components/vehicle/VehicleModal.tsx
@@ -13,6 +13,8 @@ import {
   Box,
   Image,
 } from '@chakra-ui/react';
+import { ImBin } from 'react-icons/im';
+import { FaRegEdit } from 'react-icons/fa';
 
 interface ModalFormProps {
   isOpen: boolean;
@@ -27,49 +29,68 @@ const ModalForm: React.FC<ModalFormProps> = ({ isOpen, onClose }) => {
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay/>
-      <ModalContent paddingTop={'50px'} maxW='800px'>
+      <ModalOverlay />
+      <ModalContent paddingTop={'50px'} maxW="800px">
         <ModalCloseButton />
         <Box display="flex" justifyContent="center" alignItems="center">
-      <Box display="flex" alignItems="center">
-        <Image src="/images/modalvehicle.svg" width={'100px'} marginRight={'60px'} />
-        <Heading>Edit a Vehicle</Heading>
-      </Box>
-    </Box>
-
-        <form onSubmit={handleSubmit}>
-          <FormControl>
-            <FormLabel htmlFor="plateNumber" >PlateNumber</FormLabel>
-            <Input id="plateNumber" name="plateNumber" type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="brand">Brand</FormLabel>
-            <Input id="brand" name="brand" type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="model">Model</FormLabel>
-            <Input id="model" name="model" type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="type">Type</FormLabel>
-            <Select id="type" name="type">
-              <option value="sedan">Sedan</option>
-              <option value="suv">SUV</option>
-              <option value="truck">Truck</option>
-            </Select>
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="color">Color</FormLabel>
-            <Input id="color" name="color" type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="year">Year</FormLabel>
-            <Input id="year" name="year" type="date" />
-          </FormControl>
-          <Button type="submit" variant="blue">
-            Submit
-          </Button>
-        </form>
+          <Image src="/images/modalvehicle.svg" width={'100px'} />
+          <Heading>Edit a Vehicle</Heading>
+        </Box>
+        <Box display="flex" justifyContent="center">
+          <form onSubmit={handleSubmit}>
+            <FormControl>
+              <FormLabel marginTop="0.5rem">Brand</FormLabel>
+              <Input width={'600px'} placeholder={'Jeep'} type="text" />
+            </FormControl>
+            <FormControl>
+              <FormLabel marginTop="0.5rem">Model</FormLabel>
+              <Input width={'600px'} placeholder={'Cherokee'} type="text" />
+            </FormControl>
+            <FormControl>
+              <FormLabel marginTop="0.5rem">Type</FormLabel>
+              <Select width={'600px'} name="type">
+                <option value="sedan">Sedan</option>
+                <option value="suv">SUV</option>
+                <option value="truck">Truck</option>
+              </Select>
+            </FormControl>
+            <FormControl>
+              <FormLabel marginTop={'0.5rem'}>PlateNumber</FormLabel>
+              <Input width={'600px'} placeholder={'AS0423'} type="text" />
+            </FormControl>
+            <FormControl>
+              <FormLabel marginTop="0.5rem">Seats</FormLabel>
+              <Input width={'600px'} placeholder={'4'} type="text" />
+            </FormControl>
+            <FormControl>
+              <FormLabel marginTop="0.5rem">Year</FormLabel>
+              <Input width={'600px'} type="date" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Color</FormLabel>
+              <Input width={'600px'}placeholder={'Blue'} type="text" />
+            </FormControl>
+            <FormLabel marginTop="1rem">Image</FormLabel>
+            <Input width={'50%'} type="file"></Input>
+            <Box
+              display={'flex'}
+              justifyContent={'space-between'}
+              marginTop={'1rem'}
+              marginBottom={'2rem'}
+            >
+              <Button
+                type="submit"
+                rightIcon={<ImBin size={'20px'} />}
+                backgroundColor="#981039"
+              >
+                Delete
+              </Button>
+              <Button type="submit" rightIcon={<FaRegEdit size={'20px'} />}>
+                Submit
+              </Button>
+            </Box>
+          </form>
+        </Box>
       </ModalContent>
     </Modal>
   );
