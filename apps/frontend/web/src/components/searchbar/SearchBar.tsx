8e02fdc73cab7aa865b34d2f507b0978bcd8127f
Emmanuel PEREIRA <epereira777@gmail.com>
2023-01-04 12:06:23 +0100

    chore(front): folder and import changes + add image

---
+++ apps/frontend/web/src/components/searchbar/SearchBar.tsx
@@ -0,0 +1,133 @@
+import {
+  Button,
+  Center,
+  Divider,
+  Flex,
+  FormControl,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Select,
+} from '@chakra-ui/react';
+import { BsCalendar3, BsGeoFill, BsPeopleFill } from 'react-icons/bs';
+import { GoSearch } from 'react-icons/go';
+import { HiMusicNote } from 'react-icons/hi';
+import { SlDirection } from 'react-icons/sl';
+
+const SearchBar = () => {
+  const today = new Date();
+  const defaultDate = new Date(today).toISOString().split('T')[0];
+
+  return (
+    <>
+      <HStack w={'70%'} shadow={'md'}>
+        <Center>
+          <FormControl>
+            <HStack>
+              <Flex width={'40%'}>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<BsGeoFill color="gray.300" />}
+                  />
+                  <Input
+                    type="text"
+                    placeholder="Departure"
+                    variant="flushed"
+                  />
+                </InputGroup>
+              </Flex>
+
+              <Center height="40px">
+                <Divider
+                  orientation="vertical"
+                  borderWidth="md"
+                  borderColor={'#87B8E6'}
+                />
+              </Center>
+
+              <Flex width={'40%'}>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<SlDirection color="gray.300" />}
+                  />
+                  <Input
+                    type="text"
+                    placeholder="Destination"
+                    variant="flushed"
+                  />
+                </InputGroup>
+              </Flex>
+
+              <Center height="40px">
+                <Divider
+                  orientation="vertical"
+                  borderWidth="md"
+                  borderColor={'#87B8E6'}
+                />
+              </Center>
+
+              <Flex width={'40%'}>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<BsCalendar3 color="gray.300" />}
+                  />
+                  <Input
+                    type="date"
+                    defaultValue={defaultDate}
+                    variant="flushed"
+                  />
+                </InputGroup>
+              </Flex>
+
+              <Center height="40px">
+                <Divider
+                  orientation="vertical"
+                  borderWidth="md"
+                  borderColor={'#87B8E6'}
+                />
+              </Center>
+
+              <Flex width={'15%'}>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<BsPeopleFill color="gray.300" />}
+                  />
+                  <Input type="number" placeholder="0" variant="flushed" />
+                </InputGroup>
+              </Flex>
+
+              <Center height="40px">
+                <Divider orientation="vertical" borderColor={'#87B8E6'} />
+              </Center>
+              <HiMusicNote size={40} />
+              <Flex w="40%">
+                <Select placeholder={'Music'} variant="unstyled">
+                  <option value="Dance">Dance</option>
+                  <option value="Hip_Hop">Hip hop</option>
+                  <option value="Heavy_metal">Heavy metal</option>
+                </Select>
+              </Flex>
+
+              <Button
+                leftIcon={<GoSearch />}
+                type="submit"
+                isLoading={false}
+                loadingText="Loading"
+                w={'20%'}
+              >
+                Search
+              </Button>
+            </HStack>
+          </FormControl>
+        </Center>
+      </HStack>
+    </>
+  );
+};
+
+export default SearchBar;
--- apps/frontend/web/src/pages/Search.tsx
+++ apps/frontend/web/src/pages/Search.tsx
@@ -1,5 +1,5 @@
-import { Box, Center, Heading, HStack, VStack } from "@chakra-ui/react"
-import SearchBar from "../theme/components/SearchBar"
+import { Box, Center, Flex, Heading, HStack, Img, VStack } from "@chakra-ui/react"
+import SearchBar from "../components/searchbar/SearchBar"
 
 
 const Search = () => {
@@ -8,11 +8,16 @@ const Search = () => {
   return (
     <VStack spacing='24px' padding='24px'>
       <HStack>
-        <Box w={'100%'} padding='24px'>
-            <Center>
-              <Heading fontSize='xl'>Search for a trip</Heading>
-            </Center>
-        </Box>
+        <Flex w={'100%'} padding='24px'>
+           
+              <Flex align="center" justify="center" w={'50%'}>
+                <Img src="../../public/images/undraw_travel_booking.svg" w={'50%'} />
+              </Flex>
+              <Flex align="center" w={'50%'}>
+                <Heading fontSize='xl'>Search for a trip</Heading>
+              </Flex>
+          
+        </Flex>
       </HStack>
       
       <SearchBar />
