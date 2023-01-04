f66bc23743ef79788b33a91455e8f8a605373efe
Dangiras <dan.hausmans@gmail.com>
2023-01-04 15:52:06 +0100

    Chore(Frontend + Backend): Removed SomeValue from front and back

--- apps/frontend/web/src/App.tsx
+++ apps/frontend/web/src/App.tsx
@@ -1,25 +1,5 @@
-import { useEffect, useState } from 'react';
-import { SomeInterface, User } from '@libs/typings';
-
 function App() {
-  const [someData, setSomeData] = useState<SomeInterface>({
-    someProperty: 'someValue',
-  });
-
-  const user: Partial<User> = {};
-
-  useEffect(() => {
-    const abortController = new AbortController();
-    const go = async () => {};
-
-    go();
-
-    return () => {
-      abortController.abort();
-    };
-  }, []);
-
-  return <div className="App">{`${someData.someProperty}`}</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
