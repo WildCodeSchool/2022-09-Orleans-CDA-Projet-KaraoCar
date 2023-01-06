e9ad2b6a052d7198d9e29570db9daff2d875fa55
Emmanuel PEREIRA <epereira777@gmail.com>
2023-01-06 14:34:28 +0100

    feat(frontend): triplist and triplist details

--- apps/frontend/web/src/components/searchbar/SearchBar.tsx
+++ apps/frontend/web/src/components/searchbar/SearchBar.tsx
@@ -8,6 +8,11 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
   Select,
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
@@ -18,23 +23,92 @@ import { SlDirection } from 'react-icons/sl';
 import SearchCriteria from '../../interfaces/SearchCriteriaInterface';
 import TripList from '../triplist/TripList';
 
-
 const SearchBar = (searchCriteria: SearchCriteria) => {
   const today = new Date();
   const defaultDate = new Date(today).toISOString().split('T')[0];
   const [criteria, setCriteria] = useState<SearchCriteria>({});
+  const [musicalChoice, setMusicalChoice] = useState<number>(0); //id of the musical choice
+  const [nbPassengers, setNbPassengers] = useState<number>(1);
+  const [departure, setDeparture] = useState<string>('');
+  const [destination, setDestination] = useState<string>('');
+  const [dateDeparture, setDateDeparture] = useState<string>();
+  const [musicalStyle, setMusicalStyle] = useState<
+    { id: number; name: string }[]
+  >([]);
 
   useEffect(() => {
-    searchCriteria ?
-      setCriteria(searchCriteria): 
-      setCriteria({dateDeparture: defaultDate});
+    setDateDeparture(
+      searchCriteria?.dateDeparture
+        ? searchCriteria?.dateDeparture
+        : defaultDate
+    );
 
-  }, [])
-  
-  useEffect(()=> {
-    console.log('criteres')
+    setCriteria({
+      departure: searchCriteria?.departure ? searchCriteria?.departure : '',
+      destination: searchCriteria?.destination
+        ? searchCriteria?.destination
+        : '',
+      dateDeparture: searchCriteria?.dateDeparture
+        ? searchCriteria?.dateDeparture
+        : defaultDate,
+      musicalChoice: searchCriteria?.musicalChoice
+        ? {
+            id: searchCriteria?.musicalChoice?.id,
+            name: searchCriteria?.musicalChoice?.name,
+          }
+        : { id: 0, name: '' },
+      nbPassengers: searchCriteria?.nbPassengers
+        ? searchCriteria?.nbPassengers
+        : 1,
+    });
+
+    const musicalStyle = [
+      { id: 1, name: 'Rock' },
+      { id: 2, name: 'Pop' },
+      { id: 3, name: 'Rap' },
+      { id: 4, name: 'RnB' },
+      { id: 5, name: 'Jazz' },
+      { id: 6, name: 'Classique' },
+      { id: 7, name: 'Electro' },
+      { id: 8, name: 'Metal' },
+      { id: 9, name: 'Funk' },
+      { id: 10, name: 'Reggae' },
+      { id: 11, name: 'Soul' },
+      { id: 12, name: 'Blues' },
+      { id: 13, name: 'Country' },
+      { id: 14, name: 'Folk' },
+      { id: 15, name: 'Indie' },
+      { id: 16, name: 'Hip-Hop' },
+      { id: 17, name: 'Techno' },
+      { id: 18, name: 'Disco' },
+      { id: 19, name: 'House' },
+      { id: 20, name: 'Ska' },
+      { id: 21, name: 'Punk' },
+      { id: 22, name: 'Soul' },
+    ];
+    setMusicalStyle(musicalStyle);
+  }, []);
+
+  useEffect(() => {
     console.log(criteria);
-  }, [criteria])
+  }, [criteria]);
+
+  const handleSearch = (e: Event) => {
+    e.preventDefault();
+    setCriteria({
+      departure: departure,
+      destination: destination,
+      dateDeparture: dateDeparture,
+      musicalChoice: musicalChoice
+        ? {
+            id: musicalChoice,
+            name: musicalStyle.find((style) => style.id === musicalChoice)
+              ?.name,
+          }
+        : { id: 0, name: '' },
+      nbPassengers: nbPassengers,
+    });
+  };
 
   return (
     <>
@@ -52,6 +126,8 @@ const SearchBar = (searchCriteria: SearchCriteria) => {
                     type="text"
                     placeholder="Departure"
                     variant="flushed"
+                    value={departure}
+                    onChange={(e) => setDeparture(e.target.value)}
                   />
                 </InputGroup>
               </Flex>
@@ -74,6 +150,8 @@ const SearchBar = (searchCriteria: SearchCriteria) => {
                     type="text"
                     placeholder="Destination"
                     variant="flushed"
+                    value={destination}
+                    onChange={(e) => setDestination(e.target.value)}
                   />
                 </InputGroup>
               </Flex>
@@ -94,8 +172,9 @@ const SearchBar = (searchCriteria: SearchCriteria) => {
                   />
                   <Input
                     type="date"
-                    defaultValue={defaultDate}
                     variant="flushed"
+                    defaultValue={dateDeparture ? dateDeparture : defaultDate}
+                    onChange={(e) => setDateDeparture(e.target.value)}
                   />
                 </InputGroup>
               </Flex>
@@ -109,13 +188,22 @@ const SearchBar = (searchCriteria: SearchCriteria) => {
               </Center>
 
               <Flex width={'150px'}>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<BsPeopleFill color="gray.300" />}
-                  />
-                  <Input type="number" placeholder="0" variant="flushed" />
-                </InputGroup>
+                <BsPeopleFill size={40} />
+
+                <NumberInput
+                  defaultValue={nbPassengers}
+                  min={1}
+                  max={5}
+                  variant={'flushed'}
+                  ml={'3'}
+                  onChange={(value) => setNbPassengers(+value)}
+                >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
               </Flex>
 
               <Center height="40px">
@@ -123,10 +211,16 @@ const SearchBar = (searchCriteria: SearchCriteria) => {
               </Center>
               <HiMusicNote size={40} />
               <Flex w="40%">
-                <Select placeholder={'Music'} variant="unstyled">
-                  <option value="Dance">Dance</option>
-                  <option value="Hip_Hop">Hip hop</option>
-                  <option value="Heavy_metal">Heavy metal</option>
+                <Select
+                  placeholder={'Music'}
+                  variant="unstyled"
+                  onChange={(e) => setMusicalChoice(+e.target.value)}
+                >
+                  {musicalStyle?.map((style) => (
+                    <option key={style?.id} value={style?.id}>
+                      {style?.name}
+                    </option>
+                  ))}
                 </Select>
               </Flex>
 
@@ -136,6 +230,7 @@ const SearchBar = (searchCriteria: SearchCriteria) => {
                 isLoading={false}
                 loadingText="Loading"
                 w={'250px'}
+                onClick={handleSearch}
               >
                 Search
               </Button>
--- apps/frontend/web/src/components/triplist/TripList.tsx
+++ apps/frontend/web/src/components/triplist/TripList.tsx
@@ -3,6 +3,7 @@ import {
   Card,
   CardBody,
   CardHeader,
+  Center,
   Divider,
   Flex,
   Grid,
@@ -10,16 +11,35 @@ import {
   Heading,
   Image,
   Img,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spacer,
   Stack,
+  Text,
+  useDisclosure,
 } from '@chakra-ui/react';
+import { useEffect } from 'react';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { BiCar } from 'react-icons/bi';
-import { BsGeoFill, BsSignpostFill } from 'react-icons/bs';
+import { BsCheckSquare, BsGeoFill, BsMusicNoteList, BsSignpostFill } from 'react-icons/bs';
 import { GiSteeringWheel } from 'react-icons/gi';
-import { MdFormatListBulleted } from 'react-icons/md';
+import { HiOutlineCake, HiOutlineUsers } from 'react-icons/hi';
+import { MdFormatListBulleted, MdOutlineDescription } from 'react-icons/md';
 
 const TripList = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    console.log('TripList');
+  }, []);
+
+
+
   return (
     <Flex w={'70%'}>
       <Stack spacing="10" w={'100%'}>
@@ -136,9 +156,165 @@ const TripList = () => {
               </GridItem>
               <GridItem colSpan={2}>
                 <Flex align={'center'} h={'100%'} direction={'row-reverse'}>
-                  <Button size={'sm'} rightIcon={<MdFormatListBulleted />}>
-                    Détails
+                  <Button size={'sm'} rightIcon={<MdFormatListBulleted />} onClick={onOpen}>
+                    Details
                   </Button>
+
+                  <Modal isOpen={isOpen} onClose={onClose} size={'4xl'}>
+                    <ModalOverlay />
+                    <ModalContent>
+                      <ModalHeader bgColor={'#394E61'}
+                        color={'#FFFFFF'}
+                        borderTopRadius={'md'}>
+                          <Flex direction={'row'} justify={'space-between'}>
+                            <Flex>To Blois</Flex>
+                            <Flex mr={16}>{'15H15 -> 17H15'}</Flex>
+                            <Flex></Flex>
+                          </Flex>
+                      </ModalHeader>
+                      <ModalCloseButton />
+                      <ModalBody>
+                        <Flex>
+                          <Flex direction={'column'} w={'100%'} p={6} align={'center'}>
+                            <Img src={'../../../public/images/undraw_by_my_car_gicy.svg'} />
+                            <Flex mt={'4'}>
+                              <Text as='b' fontSize={'lg'}>Car model</Text>
+                              
+                            </Flex>
+                            <Flex>
+                              <Flex mt={'1'}><BiCar/></Flex><Text ml={'1'} mr={'1'} as='b'>Type:</Text>type 
+                              <Flex mt={'1'} ml={'4'}><BiCar/></Flex><Text ml={'1'} mr={'1'} as='b'>Model:</Text> model 
+                              
+                            </Flex>
+
+                          </Flex>
+                          <Flex direction={'column'} p={4} w={'50%'}>
+                            <Image
+                              borderRadius='full'
+                              boxSize='150px'
+                              src='../../../public/images/profile-placeholder.png'                       />
+                          </Flex>
+
+                          <Flex direction={'column'} w={'100%'}>
+                            <Flex direction={'row'}>
+                              <Flex p={2} mt={'2'}><GiSteeringWheel size={'30'}/></Flex>
+                              <Flex p={2} mt={'2'} direction={'column'}>
+                                <Flex>
+                                  <Text as='b'>Name Surname</Text>
+                                </Flex>
+                                <Flex align={'center'}>
+                                  <AiFillStar color="#FDBC4B" />
+                                  <AiFillStar color="#FDBC4B" />
+                                  <AiFillStar color="#FDBC4B" />
+                                  <AiFillStar color="#FDBC4B" />
+                                  <AiOutlineStar color="#FDBC4B" />
+                                  <Text ml={'2'}>0 vote</Text>
+                                </Flex>
+
+                              </Flex>
+                            </Flex> 
+
+                            <Flex direction={'row'}>
+                              <Flex p={2} mt={'2'}><HiOutlineCake size={'30'}/></Flex>
+                              <Flex p={2} mt={'2'} direction={'column'}>
+                                <Flex mt={'1'}>
+                                  00 years old
+                                </Flex>
+                              </Flex>
+                            </Flex> 
+
+                            <Flex direction={'row'}>
+                              <Flex p={2} mt={'2'}><MdOutlineDescription size={'30'}/></Flex>
+                              <Flex p={2} mt={'2'} direction={'column'}>
+                                <Flex mt={'1'}>
+                                  Lorem ipsum dolor sit Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit accusamus possimus cupiditate tenetur illo quae, cumque eveniet quo perferendis laborum ex. Minus quod saepe, labore ex ut corporis vero! Impedit?
+                                </Flex>
+                              </Flex>
+                            </Flex>
+
+                          </Flex>
+                  
+                        </Flex>
+                        <Flex p={'4'} align={'center'}>
+                          <Flex w={'50%'}>
+                            <Img src={'../../../public/images/undraw_map_dark_re_36sy.svg'}  />
+
+                          </Flex>
+                          <Flex p={'4'} direction={'column'}>
+                            <Flex>
+                              <Flex mr={'2'} mt={'1'}><BsGeoFill />
+                              </Flex>
+                              <Flex>
+                                3 avenue du champ de Mars, 45 000 ORLEANS
+                              </Flex>
+                            </Flex>
+                            <Flex>
+                              <Flex mr={'2'} align={'center'} mt={'1'} justify={'center'} w={'16px'}>
+                              <Divider
+                                orientation="vertical"
+                                borderWidth="md"
+                                borderColor={'#87B8E6'}
+                              />
+                              </Flex>
+                              <Flex direction={'column'}>
+                                <Flex mt={'2'}>
+                                  <Flex mt={'1'}><BsMusicNoteList /></Flex><Flex ml={'2'}>Music: </Flex>
+                                </Flex>
+                                <Flex mt={'2'}>
+                                  <Flex mt={'1'}><HiOutlineUsers /> </Flex><Flex ml={'2'}>Passengers: No middleseat</Flex>
+                                </Flex>
+                                <Flex my={'2'} p={'4'}>
+                                  <Flex direction={'column'} align={'center'} p={'2'}>
+                                    <Image
+                                      borderRadius="full"
+                                      boxSize="60px"
+                                      src="../../../public/images/profile-placeholder.png"
+                                    />
+                                    <Text fontSize={'sm'}>name s.</Text>
+                                  </Flex>
+                                  <Flex direction={'column'} align={'center'} p={'2'}>
+                                    <Image
+                                      borderRadius="full"
+                                      boxSize="60px"
+                                      src="../../../public/images/profile-placeholder.png"
+                                    />
+                                    <Text fontSize={'sm'}>name s.</Text>
+                                  </Flex>
+                                  <Flex direction={'column'} align={'center'} p={'2'}>
+                                    <Image
+                                      borderRadius="full"
+                                      boxSize="60px"
+                                      src="../../../public/images/profile-placeholder.png"
+                                    />
+                                    <Text fontSize={'sm'}>name s.</Text>
+                                  </Flex>
+                                  
+                                </Flex>
+                                
+                              </Flex>
+                            </Flex>
+                            <Flex>
+                              <Flex mr={'2'} mt={'1'}><BsSignpostFill />
+                              </Flex>
+                              <Flex>
+                                20 place de la République, 41 000 BLOIS
+                              </Flex>
+                            </Flex>
+                          </Flex>
+                        </Flex>
+                      </ModalBody>
+
+                      <Center>
+                        <ModalFooter>
+                          <Button onClick={onClose}>
+                            <Flex mr={'2'}>Book trip </Flex><BsCheckSquare />
+                          </Button>
+                        </ModalFooter>
+                      </Center>
+                    </ModalContent>
+                  </Modal>
+
+                  
                 </Flex>
               </GridItem>
             </Grid>
